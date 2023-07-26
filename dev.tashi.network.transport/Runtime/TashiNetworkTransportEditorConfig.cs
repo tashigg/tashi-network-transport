4d931d27686641274404bde6862d3d667005cd5e
Steven Joruk <steven.joruk@launchbadge.com>
2023-07-26 16:51:24 +0100

    fix: default to a valid Tashi Relay URL

--- dev.tashi.network.transport/Runtime/TashiNetworkTransportEditorConfig.cs
+++ dev.tashi.network.transport/Runtime/TashiNetworkTransportEditorConfig.cs
@@ -17,7 +17,7 @@ namespace Tashi.NetworkTransport
         public TashiNetworkMode NetworkMode = TashiNetworkMode.TashiRelay;
 
         [Tooltip("If using Tashi Relay, supply the base URL you were given here.")]
-        public string TashiRelayBaseUrl = "https://relay.tashi.gg";
+        public string TashiRelayBaseUrl = "https://eastus.relay.infra.tashi.dev";
 
         [Tooltip("If using Tashi Relay, supply the API key you were given here.")]
         public string TashiRelayApiKey;
